9d8bce0b40637803608655f4f5ff33cfe941322f
kanchanepally <kanchanepally@gmail.com>
2026-05-14 21:04:34 +0100

    fix(marketing): fix next.js manifest icon purpose type error

--- marketing/app/manifest.ts
+++ marketing/app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/api/icon?size=192',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       },
       {
         src: '/api/icon?size=512',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       },
     ],
   }
